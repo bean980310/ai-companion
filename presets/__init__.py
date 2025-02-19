51be38c381e4fb4547f33bb5c3f7f3f703ece64e
bean980310 <bean980310@hotmail.co.kr>
2025-02-19 20:59:10 +0900

    stable_diffusion_prompt_generator_preset.py의 이름을 stable_diffusion_image_generator_preset.py로 변경

--- presets/__init__.py
+++ presets/__init__.py
@@ -2,7 +2,7 @@ from .ai_assistant_preset import AI_ASSISTANT_PRESET
 from .minami_asuka_char_preset import MINAMI_ASUKA_PRESET
 from .makotono_aoi_char_preset import MAKOTONO_AOI_PRESET
 from .aino_koito_char_preset import AINO_KOITO_PRESET
-from .stable_diffusion_prompt_generator_preset import SD_IMAGE_GENERATOR_PRESET
+from .stable_diffusion_image_generator_preset import SD_IMAGE_GENERATOR_PRESET
 
 __all__=[
     'AI_ASSISTANT_PRESET',
