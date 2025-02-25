670501b436db9436893bfe3096fe979e9dc19492
bean980310 <bean980310@hotmail.co.kr>
2025-02-25 09:06:00 +0900

    캐릭터 문서 통일(전처리로 문단 분리 예정)

--- src/rag/rag.py
+++ src/rag/rag.py
@@ -1,6 +1,10 @@
+import os
+import re
+from langchain.docstore.document import Document
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 
-loader = DirectoryLoader(path='characters_info', glob="**/*.txt", loader_cls=TextLoader)
+loader = DirectoryLoader(path='characters_info', glob="*.txt", loader_cls=TextLoader)
 
 data=loader.load()
+data=data.sort(key=lambda x: x.metadata['source'].split('/')[-1])
 len(data)
---
+++ src/rag/rag_mlx.py
@@ -0,0 +1,3 @@
+minami_asuka_info="characters_info/minami_asuka.txt"
+makotono_aoi_info="characters_info/makotono_aoi.txt"
+aino_koito_info="characters_info/aino_koito.txt"
