411134c39a396121a5e874246c487a8e1504033a
bean980310 <bean980310@hotmail.co.kr>
2025-02-27 13:49:08 +0900

    캐릭터 변경시 시스템 프롬프트를 제대로 반환하지 못하는 문제 수정

--- src/characters/preset_images.py
+++ src/characters/preset_images.py
@@ -1,9 +1,9 @@
 import os
 
 PRESET_IMAGES = {
-    "AI_ASSISTANT_PRESET": os.path.join("assets", "0_ai_assistant.png"),
-    "MINAMI_ASUKA_PRESET": os.path.join("assets", "1_minami_asuka.png"),
-    "MAKOTONO_AOI_PRESET": os.path.join("assets", "2_makotono_aoi.png"),
-    "AINO_KOITO_PRESET": os.path.join("assets", "3_aino_koito.png"),
-    "SD_IMAGE_GENERATOR_PRESET": os.path.join("assets", "Z_image_generator.png")
+    "AI 비서(AI Assistant)": os.path.join("assets", "0_ai_assistant.png"),
+    "Image Generator": os.path.join("assets", "Z_image_generator.png"),
+    "미나미 아스카 (南飛鳥, みなみあすか, Minami Asuka)": os.path.join("assets", "1_minami_asuka.png"),
+    "마코토노 아오이 (真琴乃葵, まことのあおい, Makotono Aoi)": os.path.join("assets", "2_makotono_aoi.png"),
+    "아이노 코이토 (愛野小糸, あいのこいと, Aino Koito)": os.path.join("assets", "3_aino_koito.png"),
 }
--- src/common/character_info.py
+++ src/common/character_info.py
@@ -3,35 +3,35 @@ characters={
         "default_tone": "존댓말",
         "languages": ["ko", "ja", "zh_CN", "zh_TW", "en"],
         "default_language": "ko",
-        "preset_name": "AI_ASSISTANT_PRESET",
+        "preset_name": "AI 비서(AI Assistant)",
         "profile_image": "assets/0_ai_assistant.png"
     },
     "Image Generator": {
         "default_tone": "formal",
         "languages": ["en"],
         "default_language": "en",
-        "preset_name": "SD_IMAGE_GENERATOR_PRESET",
+        "preset_name": "Image Generator",
         "profile_image": "assets/Z_image_generator.png"
     },
     "미나미 아스카 (南飛鳥, みなみあすか, Minami Asuka)": {
         "default_tone": "반말", 
         "languages": ["ko", "ja", "zh_CN", "zh_TW", "en"],
         "default_language": "ko",
-        "preset_name": "MINAMI_ASUKA_PRESET",
+        "preset_name": "미나미 아스카 (南飛鳥, みなみあすか, Minami Asuka)",
         "profile_image": "assets/1_minami_asuka.png"
     },
     "마코토노 아오이 (真琴乃葵, まことのあおい, Makotono Aoi)": {
         "default_tone": "반말", 
         "languages": ["ko", "ja", "zh_CN", "zh_TW", "en"],
         "default_language": "ko",
-        "preset_name": "MAKOTONO_AOI_PRESET",
+        "preset_name": "마코토노 아오이 (真琴乃葵, まことのあおい, Makotono Aoi)",
         "profile_image": "assets/2_makotono_aoi.png"
     },
     "아이노 코이토 (愛野小糸, あいのこいと, Aino Koito)": {
         "default_tone": "반말", 
         "languages": ["ko", "ja", "zh_CN", "zh_TW", "en"],
         "default_language": "ko",
-        "preset_name": "AINO_KOITO_PRESET",
+        "preset_name": "아이노 코이토 (愛野小糸, あいのこいと, Aino Koito)",
         "profile_image": "assets/3_aino_koito.png"
     },
 }
