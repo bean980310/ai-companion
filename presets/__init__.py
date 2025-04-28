fe05827e8dc14f0cf502b05ae264a0d001bcaaff
Hyunbeen Chang <bean980310@hotmail.co.kr>
2025-04-28 22:40:33 +0900

    Character preset read json

--- presets/__init__.py
+++ presets/__init__.py
@@ -1,9 +1,10 @@
 from .ai_assistant_preset import AI_ASSISTANT_PRESET
-from .minami_asuka_char_preset import MINAMI_ASUKA_PRESET
-from .makotono_aoi_char_preset import MAKOTONO_AOI_PRESET
-from .aino_koito_char_preset import AINO_KOITO_PRESET
-from .aria_princess_fate_char_preset import ARIA_PRINCESS_FATE_PRESET
-from .aria_prince_fate_char_preset import ARIA_PRINCE_FATE_PRESET
+# from .minami_asuka_char_preset import MINAMI_ASUKA_PRESET
+# from .makotono_aoi_char_preset import MAKOTONO_AOI_PRESET
+# from .aino_koito_char_preset import AINO_KOITO_PRESET
+# from .aria_princess_fate_char_preset import ARIA_PRINCESS_FATE_PRESET
+# from .aria_prince_fate_char_preset import ARIA_PRINCE_FATE_PRESET
+from .character_preset import MINAMI_ASUKA_PRESET, MAKOTONO_AOI_PRESET, AINO_KOITO_PRESET, ARIA_PRINCESS_FATE_PRESET, ARIA_PRINCE_FATE_PRESET
 from .stable_diffusion_image_generator_preset import SD_IMAGE_GENERATOR_PRESET
 
 __all__=[
---
+++ presets/character_preset.py
@@ -0,0 +1,16 @@
+import json
+
+with open('minami_asuka_preset.json', 'r') as f:
+    MINAMI_ASUKA_PRESET = json.load(f)
+    
+with open('makotono_aoi_preset.json', 'r') as f:
+    MAKOTONO_AOI_PRESET = json.load(f)
+    
+with open('aino_koito_preset.json', 'r') as f:
+    AINO_KOITO_PRESET = json.load(f)
+    
+with open('aria_prince_fate_preset.json', 'r') as f:
+    ARIA_PRINCE_FATE_PRESET = json.load(f)
+    
+with open('aria_princess_fate_preset.json', 'r') as f:
+    ARIA_PRINCESS_FATE_PRESET = json.load(f)
