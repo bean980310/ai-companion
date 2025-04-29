1eabbef365ac0e6bfd9190d192a454217735292c
Hyunbeen Chang <bean980310@hotmail.co.kr>
2025-04-29 13:39:17 +0900

    Fix FileNotFoundError

--- presets/character_preset.py
+++ presets/character_preset.py
@@ -1,16 +1,16 @@
 import json
 
-with open('minami_asuka_preset.json', 'r') as f:
+with open('presets/minami_asuka_preset.json', 'r') as f:
     MINAMI_ASUKA_PRESET = json.load(f)
     
-with open('makotono_aoi_preset.json', 'r') as f:
+with open('presets/makotono_aoi_preset.json', 'r') as f:
     MAKOTONO_AOI_PRESET = json.load(f)
     
-with open('aino_koito_preset.json', 'r') as f:
+with open('presets/aino_koito_preset.json', 'r') as f:
     AINO_KOITO_PRESET = json.load(f)
     
-with open('aria_prince_fate_preset.json', 'r') as f:
+with open('presets/aria_prince_fate_preset.json', 'r') as f:
     ARIA_PRINCE_FATE_PRESET = json.load(f)
     
-with open('aria_princess_fate_preset.json', 'r') as f:
+with open('presets/aria_princess_fate_preset.json', 'r') as f:
     ARIA_PRINCESS_FATE_PRESET = json.load(f)
