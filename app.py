acd46dc00cb9aaf8d822676b6bcd9247bbdd52e8
Hyunbeen Chang <bean980310@hotmail.co.kr>
2025-05-21 21:42:38 +0900

    Edit macOS x86_64 supports removed error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from src.gradio_ui import demo
 if __name__=="__main__":
     print(f"Detected OS: {os_name}, Architecture: {arch}")
     if os_name == "Darwin" and arch == "x86_64":
-        raise EnvironmentError("ERROR: Support and compatibility for Intel CPU Based Mac is discontinued.")
+        raise EnvironmentError("ERROR: AI Companion for Local Machines no longer supports Intel CPU-based Macs.\nIf you are using an Intel CPU-based Macs, we recommend that you consider migrating to an Apple Silicon Based Macs or a Windows PC or Linux machine with an Nvidia GPU environment. If you have difficulty migrating from an Intel CPU-based Macs, you can use a companion application that supports Intel CPU-based Macs instead.")
     initialize_app()
 
     demo.queue().launch(debug=args.debug, share=args.share, inbrowser=args.inbrowser, server_port=args.port, width=800)
