c98e902cbbfbab17cf0506d924bceedac02fc372
bean980310 <bean980310@hotmail.co.kr>
2025-08-09 10:06:26 +0900

    Refactory api_models variables

--- src/models/api_models.py
+++ src/models/api_models.py
@@ -1,27 +1,37 @@
-api_models = [
+api_models = []
+diffusion_api_models = []
+tts_api_models = []
+
+openai_api_models = [
     "gpt-4o",
     "gpt-4o-mini",
     "gpt-4.1",
     "gpt-4.1-mini",
     "gpt-4.1-nano",
+    "o1",
+    "o1-pro",
+    "o3-mini",
+    "o3",
+    "o3-pro",
+    "o4-mini"
+    "gpt-oss-20b",
+    "gpt-oss-120b",
     "gpt-5",
     "gpt-5-mini",
     "gpt-5-nano",
     "gpt-5-chat-latest",
-    "o1",
-    "o1-pro",
-    "gpt-oss-20b",
-    "o3-mini",
-    "o3",
-    "o3-pro",
-    "gpt-oss-120b",
-    "o4-mini",
+]
+
+anthropic_api_models = [
     "claude-3-5-haiku-latest",
     "claude-3-5-sonnet-latest",
     "claude-3-7-sonnet-latest",
     "claude-sonnet-4-0",
     "claude-opus-4-0",
     "claude-opus-4-1",
+]
+
+google_ai_api_models = [
     "gemini-2.0-flash-lite",
     "gemini-2.0-flash",
     "gemini-2.0-flash-preview-image-generation",
@@ -31,58 +41,64 @@ api_models = [
     "gemini-2.5-flash-lite",
     "gemini-2.5-flash",
     "gemini-2.5-pro",
+]
+
+perplexity_api_models = [
     "sonar",
     "sonar-pro",
     "sonar-reasoning",
     "sonar-reasoning-pro",
     "sonar-deep-research",
+]
+
+xai_api_models = [
     "grok-3-mini",
     "grok-3",
     "grok-4",
-    # "mistral-small-2501",
-    # "mistral-small-2503",
-    # "mistral-small-2506",
-    # "meta-llama/Llama-3.3-70B-Instruct",
-    # "meta-llama/Llama-4-Scout-17B-16E-Instruct",
-    # "meta-llama/Llama-4-Maverick-17B-128E-Instruct",
-    # "Qwen/Qwen2.5-VL-72B-Instruct",
-    # "Qwen/Qwen3-235B-A22B",
-    # "Qwen/Qwen3-Coder-480B-A35B-Instruct",
-    # "Qwen/Qwen3-235B-A22B-Instruct-2507",
-    # "Qwen/Qwen3-235B-A22B-Thinking-2507",
-    # "deepseek-ai/DeepSeek-V3",
-    # "deepseek-ai/DeepSeek-V3-0324",
-    # "deepseek-ai/DeepSeek-R1",
-    # "deepseek-ai/DeepSeek-R1-0528",
-    # "moonshotai/Kimi-K2-Instruct",
-    # "zai-org/GLM-4.5-Air",
-    # "zai-org/GLM-4.5",
-    # "mistralai/Magistral-Small-2507",
-    # "mistralai/Devstral-Small-2507",
-    # "mistralai/Mistral-Small-3.2-24B-Instruct-2506",
-    # 필요 시 추가
 ]
 
-diffusion_api_models=[
+api_models.extend(openai_api_models)
+api_models.extend(anthropic_api_models)
+api_models.extend(google_ai_api_models)
+api_models.extend(perplexity_api_models)
+api_models.extend(xai_api_models)
+
+openai_image_api_models = [
     "dall-e-3",
     "gpt-image-1",
+]
+
+google_image_api_models = [
     "imagen-3.0-generate-002",
     "imagen-4.0-generate-preview-06-06",
     "imagen-4.0-ultra-generate-preview-06-06"
 ]
+
+xai_image_api_models = [
+    "grok-2-image"
+]
+diffusion_api_models.append(openai_image_api_models)
+diffusion_api_models.append(google_image_api_models)
+diffusion_api_models.append(xai_image_api_models)
 
 diffusion_video_api_models=[
     "veo-2.0-generate-001",
     "veo-3.0-generate-preview"
 ]
 
-tts_api_models=[
+openai_tts_api_models = [
     "tts-1",
     "tts-1-hd",
     "gpt-4o-mini-tts",
+]
+
+google_tts_api_models = [
     "gemini-2.5-pro-preview-tts",
     "gemini-2.5-flash-preview-tts"
 ]
+
+tts_api_models.extend(openai_tts_api_models)
+tts_api_models.extend(google_tts_api_models)
 
 embedding_api_models = [
     "text-embedding-3-large",
