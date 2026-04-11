ff1942d3b4732357439982c08b9198cb490f7220
bean980310 <bean980310@hotmail.co.kr>
2026-04-11 12:31:02 +0900

    Delete ComfyUI Python Client on packages

--- packages/comfyui-python-client/src/comfy_sdk/api.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional, Dict
-
-@dataclass
-class ComfyResponse:
-    prompt_id: str
-    number: Optional[int] = None
-    node_errors: Optional[Dict] = None
--- packages/comfyui-python-client/src/comfy_sdk/resources/__init__.py
+++
@@ -1,9 +0,0 @@
-from .prompt import Prompt
-from .images import Images
-from .system import System
-from .queue import Queue
-from .models import Models
-from .templates import Templates
-from .user import Users, Userdata
-
-__all__ = ["Prompt", "Images", "System", "Queue", "Models", "Templates", "Users", "Userdata"]
